1c853b32478f64f9c51897ac5360d4c6f1a323c9
LuizaADG <55961188+LuizaADG@users.noreply.github.com>
2021-02-11 22:52:49 -0300

    Sétimo período parte um

---
+++ 7 periodo/Topicos 4/trab1/trab2.py
@@ -0,0 +1,31 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+%load_ext autoreload
+%autoreload 2
+
+np.random.seed(1)
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print ("Number of training examples: " + str(m_train))
+print ("Number of testing examples: " + str(m_test))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_x_orig shape: " + str(train_x_orig.shape))
+print ("train_y shape: " + str(train_y.shape))
+print ("test_x_orig shape: " + str(test_x_orig.shape))
+print ("test_y shape: " + str(test_y.shape))
+
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T  
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
+
+print ("train_x's shape: " + str(train_x.shape))
+print ("test_x's shape: " + str(test_x.shape))
